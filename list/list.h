79fdea5d861b30dad782765a853967d04e6cdb20
Wade Bonkowski <wadeb@umich.edu>
2015-05-31 21:06:22 -0400

    Implemented and tested functions for inserting and removing from the list, both from the tail and the head.
    Also added a function to test list emptiness.
    
    Changed the prototypes of the functions to be more
    pointer-centric as this makes using the functions and
    macro much more consistent.
    
    Instinct is to try and think of a means of allowing
    users of the list to not have to use naked mallocs and frees,
    but that is not the way of C...

--- list/list.h
+++ list/list.h
@@ -20,28 +20,99 @@ struct list_entry {
 };
 
 /*
- ************************************
- * Macros for interacting with list *
- ************************************
+ **************************************************
+ * Macros and functions for interacting with list *
+ **************************************************
  */
 
 /*
  * Gets the containing struct of this list entry
- * entry: list_entry that we want to *dereference*
- * type: type of the containging struct the entry is embedded in
+ * ptr: list_entry* that we want to *dereference*
+ * type: type of the containing struct the entry is embedded in
  * member: name of the struct field that is the list link.
  */
 
-#define list_item(entry, type, member)	\
-	((type *)((char *)(&entry) - (unsigned long)(&((type *)0)->member)))
+#define list_item(ptr, type, member)	\
+	((type *)((char *)(ptr) - (unsigned long)(&((type *)0)->member)))
 
 /*
  * Initializes the list head
+ * head: list_entry* that is the head of the list
  */
 
-#define list_init(head)				\
-	do {					\
-		head.next = head.prev = &head;	\
-	} while (0)
+inline void list_init(struct list_entry *head)
+{
+	head->next = head->prev = head;
+}
+
+/*
+ * Returns 1 if the list is empty, 0 otherwise
+ * head: list_entry* that is the head of the list
+ */
+
+inline int list_empty(struct list_entry *head)
+{
+	return ((head->next == head) && (head->prev == head));
+}
+
+/*
+ * Insert element at the tail of the list
+ * head: list_entry* that is the head of the list
+ * new_entry: list_entry* that we are inserting
+ */
+
+inline void list_insert_tail(struct list_entry *head,
+			     struct list_entry *new_entry)
+{
+	head->prev->next = new_entry;
+	new_entry->prev = head->prev;
+	new_entry->next = head;
+	head->prev = new_entry;
+}
+
+/*
+ * Insert element at the head of the list
+ * head: list_entry* that is the head of the list
+ * new_entry: list_entry* that we are inserting
+ */
+
+inline void list_insert_head(struct list_entry *head,
+			     struct list_entry *new_entry)
+{
+	head->next->prev = new_entry;
+	new_entry->next = head->next;
+	new_entry->prev = head;
+	head->next = new_entry;
+}
+
+/*
+ * Removes and returns pointer to the element at the head of the list
+ * head: list_entry* that is the head of the list
+ */
+
+inline struct list_entry* list_remove_head(struct list_entry *head)
+{
+	struct list_entry *target = head->next;
+	
+	head->next = target->next;
+	target->next->prev = head;
+
+	return target;
+}
+
+/*
+ * Removes and returns pointer to the element at the tail of the list
+ * head: list_entry* that is the head of the list
+ */
+
+inline struct list_entry* list_remove_tail(struct list_entry *head)
+{
+	struct list_entry *target = head->prev;
+
+	head->prev = target->prev;
+	target->prev->next = head;
+
+	return target;
+}
 
 #endif /* _LIST_H_ */
--- list/tests/init.c
+++ list/tests/init.c
@@ -1,27 +1,46 @@
 #include "../list.h"
+
 #include <stdio.h>
+#include <stdlib.h>
 
 /* test struct definition */
-struct three_numbers {
-	int one, two, three;
-	struct list_entry list_head;
+struct integer {
+	int num;
+	struct list_entry linking;
 };
 
 int main(void)
 {
-	/* mainly a test for compilation */
-	struct three_numbers test, *result;
-	test.one = test.two = test.three = 0;
-	list_init(test.list_head);
+	struct list_entry *list_head = malloc(sizeof(struct list_entry));
+	struct list_entry *iterator;
+	struct integer *tmp_int;
+	int i;
 
-	/* just cause we can, test list_item */
-	result = list_item(test.list_head, struct three_numbers, list_head);
-
-	if (result == &test) {
-		printf("Hey, it worked!\n");
-		return 0;
-	} else {
-		printf("Hey, that was an error!\n");
-		return 1;
+	/* initialize the list */
+	list_init(list_head);
+	
+	/* insert #'s 1-10 into the list */
+	for (i = 0; i < 10; i++) {
+		tmp_int = malloc(sizeof(struct integer));
+		tmp_int->num = i;
+		list_insert_tail(list_head, &tmp_int->linking);
 	}
+
+	/* iterate through the list and print the numbers */
+	iterator = list_head->next;
+	while (iterator != list_head) {
+		tmp_int = list_item(iterator, struct integer, linking);
+		printf("%d\n", tmp_int->num);
+		iterator = iterator->next;
+	}
+
+	/* cleanup our memory mess... */
+	while (!list_empty(list_head)) {
+		iterator = list_remove_tail(list_head);
+		tmp_int = list_item(iterator, struct integer, linking);
+		free(tmp_int);
+	}
+	free(list_head);
+
+	return 0;
 }
