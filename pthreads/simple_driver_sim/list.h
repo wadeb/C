7065184bc3c4b951ed16e70dd312e91202c735b9
Wade Bonkowski <wadeb@umich.edu>
2015-06-05 05:00:16 -0400

    Started working on the simple driver model that is based off of pchen's project 1 from EECS 482.
    I have all of the code laid out except for an implementation
    of the servicer function which is the consumer of queue
    entries.
    
    I also have noted some things that I might want to change
    about after I get finished with the first version

---
+++ pthreads/simple_driver_sim/list.h
@@ -0,0 +1,159 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+
+/* 
+ * - Wade Bonkowski 05/30/2015 -
+ *
+ * Implemenation of a doubly-linked list in C.
+ * To use, imbed a linking field into the structure
+ * that you want to put into the list.
+ * To maintain you should only need to keep a reference
+ * to the list head.
+ *
+ * The list is circularly linked with a list head that is the
+ * same list entry structure.
+ *
+ */
+
+struct list_entry {
+	struct list_entry *next, *prev;
+};
+
+/*
+ **************************************************
+ * Macros and functions for interacting with list *
+ **************************************************
+ */
+
+/*
+ * Gets the containing struct of this list entry
+ * ptr: list_entry* that we want to *dereference*
+ * type: type of the containing struct the entry is embedded in
+ * member: name of the struct field that is the list link.
+ */
+
+#define list_item(ptr, type, member)	\
+	((type *)((char *)(ptr) - (unsigned long)(&((type *)0)->member)))
+
+/*
+ * Initializes the list head
+ * head: list_entry* that is the head of the list
+ */
+
+inline void list_init(struct list_entry *head)
+{
+	head->next = head->prev = head;
+}
+
+/*
+ * Returns 1 if the list is empty, 0 otherwise
+ * head: list_entry* that is the head of the list
+ */
+
+inline int list_empty(struct list_entry *head)
+{
+	return ((head->next == head) && (head->prev == head));
+}
+
+/*
+ * Insert element at the tail of the list
+ * head: list_entry* that is the head of the list
+ * new_entry: list_entry* that we are inserting
+ */
+
+inline void list_insert_tail(struct list_entry *head,
+			     struct list_entry *new_entry)
+{
+	head->prev->next = new_entry;
+	new_entry->prev = head->prev;
+	new_entry->next = head;
+	head->prev = new_entry;
+}
+
+/*
+ * Insert element at the head of the list
+ * head: list_entry* that is the head of the list
+ * new_entry: list_entry* that we are inserting
+ */
+
+inline void list_insert_head(struct list_entry *head,
+			     struct list_entry *new_entry)
+{
+	head->next->prev = new_entry;
+	new_entry->next = head->next;
+	new_entry->prev = head;
+	head->next = new_entry;
+}
+
+/*
+ * Removes and returns pointer to the element at the head of the list
+ * head: list_entry* that is the head of the list
+ */
+
+inline struct list_entry* list_remove_head(struct list_entry *head)
+{
+	struct list_entry *target = head->next;
+	
+	head->next = target->next;
+	target->next->prev = head;
+
+	return target;
+}
+
+/*
+ * Removes and returns pointer to the element at the tail of the list
+ * head: list_entry* that is the head of the list
+ */
+
+inline struct list_entry* list_remove_tail(struct list_entry *head)
+{
+	struct list_entry *target = head->prev;
+
+	head->prev = target->prev;
+	target->prev->next = head;
+
+	return target;
+}
+
+/*
+ * Removes element at position of iterator from the list it is in
+ * target: list_entry* that we want to remove
+ */
+
+inline void list_remove(struct list_entry *target)
+{
+	target->prev->next = target->next;
+	target->next->prev = target->prev;
+}
+
+/*
+ * Inserts new_entry after the list_entry* position in the list
+ * position: list_entry* that we want to insert after
+ * new_entry: list_entry* that we are inserting
+ */
+
+inline void list_insert_after(struct list_entry *position,
+			      struct list_entry *new_entry)
+{
+	new_entry->next = position->next;
+	new_entry->prev = position;
+	position->next->prev = new_entry;
+	position->next = new_entry;
+}
+
+/*
+ * Isert new_entry before the list_entry* position in the list
+ * position: list_entry* that we want to insert before
+ * new_entry: list_entry* that we are inserting
+ */
+
+inline void list_insert_before(struct list_entry *position,
+			       struct list_entry *new_entry)
+{
+	new_entry->next = position;
+	new_entry->prev = position->prev;
+	position->prev->next = new_entry;
+	position->prev = new_entry;
+}
+
+#endif /* _LIST_H_ */
---
+++ pthreads/simple_driver_sim/request_and_service.c
@@ -0,0 +1,168 @@
+/*
+ * - Wade Bonkowski 06/5/2015 -
+ *
+ * Super simple driver-like program
+ *
+ * Exercise in using pthreads:
+ * Input to the program is n files with m integers in the file.
+ * Each integer is a request that needs to be serviced. For a request
+ * to be serviced it is to be placed into the request queue. A single
+ * servicer thread will go through the queue as fast as possible and
+ * service requests.
+ *
+ * The servicer will always service the request that has the closest
+ * integer value to the last request that as serviced in this version.
+ * Also for first iteration argc will be the number of threads.
+ * Queue size is limited to QUEUE_SIZE.
+ *
+ * Perhaps for another version I will add on another constraint.
+ * I eventually will make the threads joinable and cleanup mutexes.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+#include "list.h"
+
+#define MAX_QUEUE_SIZE 5
+
+struct requester_argument {
+	int thread_num;
+	char input_filename[255]; /* max filename length */
+};
+
+struct request {
+	int thread_num;
+	int value;
+	struct list_entry linking;
+};
+
+/* pthread shared variables */
+pthread_mutex_t queue_mutex;
+pthread_cond_t capacity_cond;
+pthread_cond_t service_cond;
+
+/*
+ * Shouldn't need a mutex for the array of cv's.
+ * The entries of the array are mutually exclusive
+ * by definition of the interations between requesters
+ * and servicers. One requester thread per finish cv.
+ */
+
+pthread_cond_t **finish_cond;
+
+/* shared queue, and its current size */
+struct list_entry queue;
+int queue_size; /* TODO: test changing this to an array */
+int num_alive_requesters;
+
+/* function to be passed to a request thread */
+void *requester(void *);
+
+/* function to be passed to the servicer thread */
+void *servicer(void *);
+
+int main(int argc, char **argv)
+{
+	/* declarations */
+	pthread_t request_threads[argc];
+	pthread_t service_thread;
+	pthread_attr_t attr;
+	int rc, i;
+	struct requester_argument *tmp;
+
+	/* initialization */
+	finish_cond = malloc(sizeof(pthread_cond_t *) * argc);
+	list_init(&queue);
+	queue_size = 0;
+	num_alive_requesters = argc - 1;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+	pthread_mutex_init(&queue_mutex, NULL);
+	pthread_cond_init(&capacity_cond, NULL);
+	pthread_cond_init(&service_cond, NULL);
+
+	/* init each finish cond cv and launch requester threads */
+	for (i = 1; i < argc; i++) {
+		tmp = malloc(sizeof(struct requester_argument));
+		tmp->thread_num = i - 1;
+		strcpy(tmp->input_filename, argv[i]);
+		finish_cond[tmp->thread_num] = malloc(sizeof(pthread_cond_t));
+		pthread_cond_init(finish_cond[tmp->thread_num], NULL);
+		rc = pthread_create(&request_threads[tmp->thread_num], &attr,
+				    requester, (void *)tmp);
+		if (rc) {
+			printf("Not able to successfully create thread.\n");
+			exit(1);
+		}
+	}
+
+	/* launch our servicer thread */
+	pthread_create(&service_thread, &attr, servicer, NULL);
+
+	/* do some cleanup and then wait */
+	pthread_attr_destroy(&attr);
+	pthread_exit(NULL);
+}
+
+/* 
+ * Read the provided file one line at a time, 
+ * as a line is acquired it is sent to the queue.
+ * We are done when we hit the end of the file
+ */
+
+void *requester(void *arg) {
+	struct requester_argument *arg_ptr = (struct requester_argument *)arg;
+	struct request *new_request;
+	FILE *data = fopen(arg_ptr->input_filename, "r");
+	char *line;
+	size_t len;
+	
+	if (!data) {
+		printf("Couldn't open one of the input files\n");
+		exit(1);
+	}
+
+	/* I will let the lock go while I read from file */
+	while (getline(&line, &len, data) != -1) {
+		pthread_mutex_lock(&queue_mutex);
+		while ((queue_size == MAX_QUEUE_SIZE) ||
+		       (queue_size == num_alive_requesters)) {
+			pthread_cond_wait(&capacity_cond, &queue_mutex);
+		}
+
+		/* we get to put something in the queue */
+		printf("Requester %d issuing request for %s\n", 
+		       arg_ptr->thread_num, line);
+		new_request = malloc(sizeof(struct request));
+		new_request->thread_num = arg_ptr->thread_num;
+		new_request->value = atoi(line);
+		list_insert_tail(&queue, &new_request->linking);
+		queue_size++;
+
+		/* tell the servicer that we put something in the queue */
+		pthread_cond_signal(&service_cond);
+
+		/* we sleep until out request is done */
+		pthread_cond_wait(finish_cond[arg_ptr->thread_num],
+				  &queue_mutex);
+
+		/* we actually don't need the lock, about to read file */
+		pthread_mutex_unlock(&queue_mutex);
+	}
+
+	/* cleanup and update global state */
+	pthread_mutex_lock(&queue_mutex);
+	num_alive_requesters--;
+	pthread_mutex_unlock(&queue_mutex);
+
+	pthread_cond_destroy(finish_cond[arg_ptr->thread_num]);
+	free(line);
+	free(arg_ptr);
+
+	pthread_exit(NULL);
+}
+
+void *servicer(void *arg) { pthread_exit(arg); }
