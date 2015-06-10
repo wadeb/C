b87abd7a8877eae86044f97191d6372d2c541b3a
Wade Bonkowski <wadeb@umich.edu>
2015-06-10 05:04:38 -0400

    It may be a bit ugly but I was able to fix the problems that I was having, and I learned a lot about how the heap behaes in C. Especially with regards to functions like realloc....
    First variadic function has been finished!

--- variadic/my_printf.c
+++ variadic/my_printf.c
@@ -14,7 +14,7 @@ int my_printf(char *, ...);
 
 void *enlarge_buffer(char *, int *);
 
-void merge_buffers(char **, char **, int *, char *, int *);
+void merge_buffers(char **, char *, int *, char **, int *);
 
 int main(void)
 {
@@ -52,12 +52,14 @@ int my_printf(char *format, ...)
 			if (*input_iter == 'd') {
 				int_arg = va_arg(arguments, int);
 				sprintf(int_buffer, "%d", int_arg);
-				merge_buffers(&buffer_iter, &int_buffer, &pos,
-					      buffer, &buffer_size);
+				merge_buffers(&buffer_iter, int_buffer, &pos,
+					      &buffer, &buffer_size);
+				input_iter++;
 			} else if (*input_iter == 's') {
 				cstr_arg = va_arg(arguments, char *);
-				merge_buffers(&buffer_iter, &cstr_arg, &pos,
-					      buffer, &buffer_size);
+				merge_buffers(&buffer_iter, cstr_arg, &pos,
+					      &buffer, &buffer_size);
+				input_iter++;
 			} else {
 				printf("Error, misuse of substitution\n");
 				rc = 1;
@@ -65,8 +67,11 @@ int my_printf(char *format, ...)
 			}
 		} else {
 			/* regular char in the format string */
-			if (!(pos % BUFFER_MULTIPLE))
+			if (!(pos % BUFFER_MULTIPLE)) {
 				buffer = enlarge_buffer(buffer, &buffer_size);
+				/* recalculate iterator */
+				buffer_iter = buffer + (pos - 1);
+			}
 
 			*buffer_iter++ = *input_iter++; pos++;
 		}
@@ -93,14 +98,17 @@ void *enlarge_buffer(char *buffer, int *buffer_size)
 
 /* Appends contents of src onto dst */
 
-void merge_buffers(char **dest, char **src, int *pos, char *buffer,
+void merge_buffers(char **dest, char *src, int *pos, char **buffer,
 		   int *buffer_size)
 {
-	while (**src) {
-		if (!(*pos % BUFFER_MULTIPLE))
-			buffer = enlarge_buffer(buffer, buffer_size);
+	while (*src) {
+		if (!(*pos % BUFFER_MULTIPLE)) {
+			*buffer = enlarge_buffer(*buffer, buffer_size);
+			/* recalculate the iterator for the new buffer */
+			*dest = *buffer + (*pos - 1);
+		}
 
-		**dest = **src;
-		(*dest)++; (*src)++; (*pos)++;
+		**dest = *src;
+		(*dest)++; src++; (*pos)++;
 	}
 }
